b9263f9e69278547aa0e4c102656ff7010f25d52
StefanOfficial <127484001+StefanOfficial@users.noreply.github.com>
2026-09-01 20:09:47 +0200

    Add files via upload

---
+++ Win 11 Pro aktiválás.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+chcp 65001
+mode con lines=20 cols=80
+color  03
+
+ECHO ------------------------------
+ECHO   Windows 11 Pro aktiválása
+ECHO ------------------------------
+ECHO          Készítette:
+ECHO         StefanOfficial
+
+ECHO.
+ECHO.
+
+ECHO Nyomj Entert az indításhoz...
+PAUSE>nul
+
+ECHO.
+ECHO Kulcs beillesztése...
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+timeout /T 3 /nobreak >nul 2>&1
+ECHO Kiszolgáló beállítása...
+slmgr /skms kms8.msguides.com
+timeout /T 3 /nobreak >nul 2>&1
+ECHO Aktiválás indítása...
+slmgr /ato
+timeout /T 3 /nobreak >nul 2>&1
+
+ECHO.
+ECHO.
+ECHO ------------------------------
+ECHO    A Windows aktiválva lett!        
+ECHO ------------------------------
+PAUSE>nul
+
+Exit
